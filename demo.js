514a6e67301dbde6f871c04e7e2536630716f7b7
sdas17 <subhamkvgms112@gmail.com>
2023-05-27 17:15:32 +0530

    nodejs express js api demo

--- demo.js
+++ demo.js
@@ -1,1 +1,11 @@
-console.log("welcoem")
+const express = require("express");
+const demo = express();
+
+const mongoose = require("./database/mongoose");
+
+// demo.listen(3000,function (){
+//     console.log(`server started on port 3000`);
+// })
+demo.listen(()=>{
+    console.log(`server started on port 3000 server started progamming `);
+})
